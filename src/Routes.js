9b9ce99cd9245c3ad10ae315ea852ca46e874c4d
Doori Kim <doori.alice.kim@gmail.com>
2019-09-16 20:34:24 +0900

    Routes에 TopBar Component 추가

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import Home from "./Pages/Home";
 import AdHome from "./Pages/AdHome";
+import TopBar from "./Components/TopBar";
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
+        <TopBar />
         <Switch>
-          <Route exact path="/home" component={AdHome} />
+          <Route exact path="/" component={AdHome} />
         </Switch>
       </Router>
     );
